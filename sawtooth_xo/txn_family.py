6cc3083769360cd434bd6a4e7581e4258fa0e9c9
Shawn T. Amundson <amundson@bitwise.io>
2016-06-14 23:28:29 -0500

    Implement apply() and check_valid()

--- sawtooth_xo/txn_family.py
+++ sawtooth_xo/txn_family.py
@@ -83,11 +83,96 @@ class XoTransaction(transaction.Transaction):
 
         LOGGER.debug('checking %s', str(self))
 
-        raise XoException('XoTransaction.check_valid is not implemented')
+        if self._name is None or self._name == '':
+            raise XoException('name not set')
+
+        if self._action is None or self._action == '':
+            raise XoException('action not set')
+
+        if self._action == 'CREATE':
+            if self._name in store:
+                raise XoException('game already exists')
+        elif self._action == 'TAKE':
+            if self._space is None:
+                raise XoException('TAKE requires space')
+
+            if self._space < 1 or self._space > 9:
+                raise XoException('invalid space')
+
+            if self._name not in store:
+                raise XoException('no such game')
+
+            state = store[self._name]['State']
+            if state in ['P1-WIN', 'P2-WIN', 'TIE']:
+                raise XoException('game complete')
+
+            if state == 'P1-NEXT' and 'Player1' in store[self._name]:
+                player1 = store[self._name]['Player1']
+                if player1 != self.OriginatorID:
+                    raise XoException('invalid player 1')
+
+            if state == 'P2-NEXT' and 'Player2' in store[self._name]:
+                player1 = store[self._name]['Player2']
+                if player1 != self.OriginatorID:
+                    raise XoException('invalid player 2')
+
+            if store[self._name]['Board'][self._space - 1] != '-':
+                raise XoException('space already taken')
+        else:
+            raise XoException('invalid action')
+
+    def _is_win(self, board, letter):
+        wins = ((1, 2, 3), (4, 5, 6), (7, 8, 9),
+                (1, 4, 7), (2, 5, 8), (3, 6, 9),
+                (1, 5, 9), (3, 5, 7))
+
+        for win in wins:
+            if (board[win[0] - 1] == letter
+                    and board[win[1] - 1] == letter
+                    and board[win[2] - 1] == letter):
+                return True
+
+        return False
 
     def apply(self, store):
         LOGGER.debug('apply %s', str(self))
-        LOGGER.error('XoTransaction.apply is not implemented')
+
+        if self._name in store:
+            game = store[self._name].copy()
+        else:
+            game = {}
+
+        if 'Board' in game:
+            board = list(game['Board'])
+        else:
+            board = list('---------')
+            state = 'P1-NEXT'
+
+        if self._space is not None:
+            if board.count('X') > board.count('O'):
+                board[self._space - 1] = 'O'
+                state = 'P1-NEXT'
+            else:
+                board[self._space - 1] = 'X'
+                state = 'P2-NEXT'
+
+            # The first time a space is taken, player 1 will be assigned.  The
+            # second time a space is taken, player 2 will be assigned.
+            if 'Player1' not in game:
+                game['Player1'] = self.OriginatorID
+            elif 'Player2' not in game:
+                game['Player2'] = self.OriginatorID
+
+        game['Board'] = "".join(board)
+        if self._is_win(game['Board'], 'X'):
+            state = 'P1-WIN'
+        elif self._is_win(game['Board'], 'O'):
+            state = 'P2-WIN'
+        elif '-' not in game['Board']:
+            state = 'TIE'
+
+        game['State'] = state
+        store[self._name] = game
 
     def dump(self):
         result = super(XoTransaction, self).dump()
